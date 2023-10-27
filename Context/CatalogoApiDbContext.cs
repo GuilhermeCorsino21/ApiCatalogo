7be696a9770fb9b77377bc23776c02e18267c96e
GuilhermeCorsino21 <guilhermecorsino21@gmail.com>
2023-10-26 23:45:46 -0300

    gerando o banco e as tabelas com o Migrations

---
+++ Context/CatalogoApiDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CatalogoApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogoApi.Context
+{
+    public class CatalogoApiDbContext : DbContext
+    {
+
+        public CatalogoApiDbContext(DbContextOptions<CatalogoApiDbContext> options) : base(options) 
+        
+        { }
+
+        public DbSet<Categoria>? Categorias { get; set; }  
+        public DbSet<Produto>? Produto { get; set; } 
+    }
+}
