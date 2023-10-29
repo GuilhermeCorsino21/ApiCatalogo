897989e0f0ee940eef00626f16d396f4a2e0fa87
GuilhermeCorsino21 <guilhermecorsino21@gmail.com>
2023-10-29 14:30:51 -0300

    criado o controlador para Produtos

---
+++ Controllers/ProdutosController.cs
@@ -0,0 +1,21 @@
+﻿using CatalogoApi.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogoApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly CatalogoApiDbContext _context;
+
+        public ProdutosController(CatalogoApiDbContext context)
+        {
+            _context = context; 
+        }
+            
+
+
+    }
+}
