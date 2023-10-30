d2224f2fbd26e01b5d49e902125ca8fbcf765e36
GuilhermeCorsino21 <guilhermecorsino21@gmail.com>
2023-10-29 21:32:46 -0300

    todos os metodos actions definidos

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -2,6 +2,7 @@
 using CatalogoApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CatalogoApi.Controllers
 {
@@ -27,7 +28,7 @@ namespace CatalogoApi.Controllers
             return produtos;
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id:int}", Name = "ObterProduto")] // get by id
 
         public ActionResult<Produto> Get(int id) 
         {
@@ -39,6 +40,57 @@ namespace CatalogoApi.Controllers
 
             return produto;    
         }
+
+        [HttpPost]
+        
+        public ActionResult Post(Produto produto)
+        {
+                        
+            if (produto is null)
+                return BadRequest();
+
+            _context.Produto.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult( "ObterProduto", new { id = produto.ProdutoId }, produto); 
+        }
+
+
+        [HttpPut("{id:int}")]
+
+        public ActionResult Put(int id, Produto produto) 
+        {
+            if(id != produto.ProdutoId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(produto);
+        
+        }
+        
+        [HttpDelete("{id:int}")]
+
+        public ActionResult Delete(int id) 
+        {
+            var produto = _context.Produto.FirstOrDefault( p => p.ProdutoId ==id);
+
+            if (produto is null) 
+            {
+                return NotFound("Produto não localizado");
+            }
+
+            _context.Produto.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);               
+
+        }
+          
+
             
 
 
--- obj/Debug/net7.0/CatalogoApi.AssemblyInfo.cs
+++ obj/Debug/net7.0/CatalogoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatalogoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+897989e0f0ee940eef00626f16d396f4a2e0fa87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e64764f229831ea72f86e42af5c37e5ae64a445")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatalogoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatalogoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
